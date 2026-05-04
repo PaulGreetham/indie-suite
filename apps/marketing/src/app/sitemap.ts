e28362b8ad56357484e5ffb806758303b53978e3
Paul Greetham <pgreetham@protonmail.com>
2026-05-04 13:47:26 +0200

    remove blog

--- apps/marketing/src/app/sitemap.ts
+++ apps/marketing/src/app/sitemap.ts
@@ -1,9 +1,7 @@
-import { getBlogPosts } from "@/lib/blog";
 import { MetadataRoute } from "next";
 import { headers } from "next/headers";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const allPosts = await getBlogPosts();
   const headersList = await headers();
   const domain = headersList.get("host") as string;
   const protocol = "https";
@@ -13,9 +11,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: `${protocol}://${domain}`,
       lastModified: new Date(),
     },
-    ...allPosts.map((post) => ({
-      url: `${protocol}://${domain}/blog/${post.slug}`,
-      lastModified: new Date(),
-    })),
   ];
 }
